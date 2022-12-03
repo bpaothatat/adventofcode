379d9ad1c0698ed9a7d5494882e85db16cd9996a
Benjamin Paothatat <bpaothat@gmail.com>
2022-12-03 11:17:53 -0700

    Add day1 part2 solution

--- day1/day1.py
+++ day1/day1.py
@@ -2,8 +2,11 @@ def getMaxCalories(filename):
     maxCalories = 0
     with open(filename) as file:
         currentCalories = 0
-        for line in file:
-            if line == '\n':
+        lines = file.readlines()
+        for index, line in enumerate(lines):
+            if line == '\n'or index == len(lines) - 1:
+                if index == len(lines) - 1:
+                    currentCalories += int(line)
                 if maxCalories < currentCalories:
                     maxCalories = currentCalories
                 currentCalories = 0
@@ -11,4 +14,30 @@ def getMaxCalories(filename):
                 currentCalories += int(line) 
     return maxCalories
 
-print(getMaxCalories('data.txt'))
+def getMaxCalories2(filename):
+    maxCalories = 0
+    secondMaxCalories = 0
+    thirdMaxCalories = 0
+    with open(filename) as file:
+        currentCalories = 0
+        lines = file.readlines()
+        for index, line in enumerate(lines):
+            if line == '\n' or index == len(lines) - 1:
+                if index == len(lines) - 1:
+                    currentCalories += int(line)
+                if maxCalories < currentCalories:
+                    thirdMaxCalories = secondMaxCalories
+                    secondMaxCalories = maxCalories
+                    maxCalories = currentCalories
+                elif secondMaxCalories < currentCalories:
+                    thirdMaxCalories = secondMaxCalories
+                    secondMaxCalories = currentCalories
+                elif thirdMaxCalories < currentCalories:
+                    thirdMaxCalories = currentCalories
+                currentCalories = 0
+            else:
+                currentCalories += int(line) 
+    return maxCalories + secondMaxCalories + thirdMaxCalories
+
+
+#print(getMaxCalories2('data.txt'))
--- day1/day1_test.py
+++ day1/day1_test.py
@@ -1,10 +1,11 @@
-from day1 import getMaxCalories
+from day1 import getMaxCalories, getMaxCalories2
 import unittest
 
 class Day1Test(unittest.TestCase):
 
     def test_get_max_calories(self):
-        self.assertTrue(getMaxCalories('example.txt'), 24000)
+        self.assertEqual(getMaxCalories('example.txt'), 24000)
+        self.assertEqual(getMaxCalories2('example.txt'), 45000)
     
 if __name__ == '__main__':
     unittest.main()
